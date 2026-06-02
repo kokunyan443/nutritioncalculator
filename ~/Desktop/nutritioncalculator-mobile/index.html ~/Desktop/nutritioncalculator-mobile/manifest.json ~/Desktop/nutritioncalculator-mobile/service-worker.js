a78a778c1eb9ab1d7382c6701b1da3c0dbf78ec8
kokunyan443 <yoshi3025.mt.island@gmail.com>
2026-06-02 23:49:14 +0900

    Update service-worker.js
    アプリ作成

--- ~/Desktop/nutritioncalculator-mobile/index.html ~/Desktop/nutritioncalculator-mobile/manifest.json ~/Desktop/nutritioncalculator-mobile/service-worker.js
+++ ~/Desktop/nutritioncalculator-mobile/index.html ~/Desktop/nutritioncalculator-mobile/manifest.json ~/Desktop/nutritioncalculator-mobile/service-worker.js
@@ -1,3 +1,6 @@
 ~/Desktop/nutritioncalculator-mobile/index.html
 ~/Desktop/nutritioncalculator-mobile/manifest.json
 ~/Desktop/nutritioncalculator-mobile/service-worker.js
+`icon-192.png`
+`icon-512.png`
+`apple-touch-icon.png`
